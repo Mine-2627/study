aba8bbaaeab2c958c9021147351387226fb4a64d
maying <1191832613@qq.com>
2021-08-01 22:05:23 +0800

    swagger测试

--- spring-boot-demo/src/main/java/com/maying/springbootdemo/controller/FileController.java
+++ spring-boot-demo/src/main/java/com/maying/springbootdemo/controller/FileController.java
@@ -1,11 +1,13 @@
 package com.maying.springbootdemo.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * description
  */
 @RestController
+@Api(tags = "文件实例")
 public class FileController {
 
 }
--- spring-boot-demo/src/main/java/com/maying/springbootdemo/controller/UserController.java
+++ spring-boot-demo/src/main/java/com/maying/springbootdemo/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.maying.springbootdemo.controller;
 
+import io.swagger.annotations.Api;
+
 /**
  * description
  */
+@Api(tags = "用户实例")
 public class UserController {
 
 }
