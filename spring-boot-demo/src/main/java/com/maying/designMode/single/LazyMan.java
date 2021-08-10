e2e60436fef36c617170aff2fa1663f26a1246ef
maying <1191832613@qq.com>
2021-08-10 15:45:31 +0800

    静态内部类

--- spring-boot-demo/src/main/java/com/maying/designMode/single/LazyMan.java
+++ spring-boot-demo/src/main/java/com/maying/designMode/single/LazyMan.java
@@ -31,9 +31,19 @@ public class LazyMan {
         }
         return lazyMan;
     }
-    //    上述为双重锁懒汉模式
-    //    如果编写的是多线程程序，则不要删除上例代码中的关键字 volatile 和 synchronized，否则将存在线程非安全的问题。
-    //    如果不删除这两个关键字就能保证线程安全，但是每次访问时都要同步，会影响性能，且消耗更多的资源
+    //   这个步骤，其实在jvm里面的执行分为三步：
+    //
+    //  1.在堆内存开辟内存空间。
+    //  2.在堆内存中实例化SingleTon里面的各个参数。
+    //  3.把对象指向堆内存空间。
+    //
+    //由于jvm存在乱序执行功能，所以可能在2还没执行时就先执行了3，如果此时再被切换到线程B上，由于执行了3，INSTANCE 已经非空了，
+    // 会被直接拿出来用，这样的话，就会出现异常。这个就是著名的DCL失效问题。
+    //
+    //不过在JDK1.5之后，官方也发现了这个问题，故而具体化了volatile，即在JDK1.6及以后，
+    // 只要定义为private volatile static SingleTon  INSTANCE = null;就可解决DCL失效问题。
+    // volatile确保INSTANCE每次均在主内存中读取，这样虽然会牺牲一点效率，但也无伤大雅
+
 
     //单线程下单例是ok的
     public static void main(String[] args) {
