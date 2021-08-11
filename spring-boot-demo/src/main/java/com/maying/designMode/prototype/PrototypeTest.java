09740f448f469384e5d17a47ba2bd1604ba636f2
maying <1191832613@qq.com>
2021-08-11 17:35:15 +0800

    原型模式-克隆

---
+++ spring-boot-demo/src/main/java/com/maying/designMode/prototype/PrototypeTest.java
@@ -0,0 +1,23 @@
+package com.maying.designMode.prototype;
+
+//具体原型类
+class Realizetype implements Cloneable {
+    Realizetype() {
+        System.out.println("具体原型创建成功！");
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        System.out.println("具体原型复制成功！");
+        return (Realizetype) super.clone();
+    }
+}
+
+//原型模式的测试类
+public class PrototypeTest {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Realizetype obj1 = new Realizetype();
+        Realizetype obj2 = (Realizetype) obj1.clone();
+        System.out.println("obj1==obj2?" + (obj1 == obj2));
+    }
+}
