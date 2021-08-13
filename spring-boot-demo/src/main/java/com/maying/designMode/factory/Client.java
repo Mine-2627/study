535a34f11feb2979d4d962382c0351baaa43fa4b
maying <1191832613@qq.com>
2021-08-13 16:59:25 +0800

    简单工厂方法

---
+++ spring-boot-demo/src/main/java/com/maying/designMode/factory/Client.java
@@ -0,0 +1,41 @@
+package com.maying.designMode.factory;
+
+/**
+ * description
+ */
+public class Client {
+    public static void main(String[] args) {
+    }
+    //抽象产品
+    public interface Product {
+        void show();
+    }
+    //具体产品：ProductA
+    static class ConcreteProduct1 implements Product {
+        public void show() {
+            System.out.println("具体产品1显示...");
+        }
+    }
+    //具体产品：ProductB
+    static class ConcreteProduct2 implements Product {
+        public void show() {
+            System.out.println("具体产品2显示...");
+        }
+    }
+    final class Const {
+        static final int PRODUCT_A = 0;
+        static final int PRODUCT_B = 1;
+        static final int PRODUCT_C = 2;
+    }
+    static class SimpleFactory {
+        public static Product makeProduct(int kind) {
+            switch (kind) {
+                case Const.PRODUCT_A:
+                    return new ConcreteProduct1();
+                case Const.PRODUCT_B:
+                    return new ConcreteProduct2();
+            }
+            return null;
+        }
+    }
+}
